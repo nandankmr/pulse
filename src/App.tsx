ff181fc3df4a1f12d0a2758dd724c660cf2f1bc3
nandan <nandankmr@gmail.com>
2025-10-11 20:43:38 +0530

    feat: add Redux store with userSlice

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,37 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
-import { Provider as PaperProvider, MD3LightTheme, MD3DarkTheme, adaptNavigationTheme } from 'react-native-paper';
-import { ThemeProvider, useTheme } from './theme/ThemeContext';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import {
+  Provider as PaperProvider,
+  MD3LightTheme,
+  MD3DarkTheme,
+} from 'react-native-paper';
+import { ThemeProvider } from './theme/ThemeContext';
 import { colors } from './theme';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const paperTheme = isDarkMode ? { ...MD3DarkTheme, colors: { ...MD3DarkTheme.colors, ...colors.dark } } : { ...MD3LightTheme, colors: { ...MD3LightTheme.colors, ...colors.light } };
+  const paperTheme = isDarkMode
+    ? { ...MD3DarkTheme, colors: { ...MD3DarkTheme.colors, ...colors.dark } }
+    : {
+        ...MD3LightTheme,
+        colors: { ...MD3LightTheme.colors, ...colors.light },
+      };
 
   return (
     <ThemeProvider>
       <PaperProvider theme={paperTheme}>
-        <SafeAreaProvider>
-          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-          <AppContent />
-        </SafeAreaProvider>
+        <Provider store={store}>
+          <SafeAreaProvider>
+            <StatusBar
+              barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            />
+            <AppContent />
+          </SafeAreaProvider>
+        </Provider>
       </PaperProvider>
     </ThemeProvider>
   );
---
+++ src/store/userSlice.ts
@@ -0,0 +1,27 @@
+// src/store/userSlice.ts
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface UserState {
+  name: string;
+  email: string;
+}
+
+const initialState: UserState = {
+  name: '',
+  email: '',
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<UserState>) => {
+      return { ...state, ...action.payload };
+    },
+    clearUser: () => initialState,
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
