7d04a88103bc2e43f780dfac6037a9bfeddfc1a9
nandan <nandankmr@gmail.com>
2025-10-11 19:49:48 +0530

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
-  SafeAreaProvider,
+      SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
