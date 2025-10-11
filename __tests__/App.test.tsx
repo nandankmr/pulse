e6e3bc8e76666e75cc75be1271a47562baf6ed8b
nandan <nandankmr@gmail.com>
2025-10-11 21:07:20 +0530

    test: fix test mocks and config

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,6 +2,42 @@
  * @format
  */
 
+jest.mock('@reduxjs/toolkit', () => ({
+  configureStore: jest.fn(),
+  createSlice: jest.fn(() => ({
+    name: 'user',
+    reducer: jest.fn(),
+    actions: {},
+  })),
+}));
+jest.mock('react-redux', () => ({
+  Provider: ({ children }) => children,
+}));
+jest.mock('react-native-paper', () => ({
+  Provider: ({ children }) => children,
+  MD3LightTheme: {},
+  MD3DarkTheme: {},
+}));
+jest.mock('../src/theme/ThemeContext', () => ({
+  ThemeProvider: ({ children }) => children,
+  useTheme: () => ({
+    colors: {
+      background: '#FFFFFF',
+      text: '#000000',
+    },
+  }),
+}));
+jest.mock('../src/navigation/AppNavigator', () => 'AppNavigator');
+jest.mock('react-native-safe-area-context', () => ({
+  SafeAreaProvider: ({ children }) => children,
+  useSafeAreaInsets: () => ({}),
+}));
+jest.mock('react-native', () => ({
+  StatusBar: 'StatusBar',
+  useColorScheme: () => 'light',
+}));
+
+// Then the test
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../src/App';
--- __tests__/HomeScreen.test.tsx
+++ __tests__/HomeScreen.test.tsx
@@ -2,9 +2,14 @@
 
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import { ThemeProvider } from '../src/theme/ThemeContext';
 import HomeScreen from '../src/screens/HomeScreen';
 
 test('renders correctly', () => {
-  const { getByText } = render(<HomeScreen />);
+  const { getByText } = render(
+    <ThemeProvider>
+      <HomeScreen />
+    </ThemeProvider>,
+  );
   expect(getByText('Welcome to Home Screen')).toBeTruthy();
 });
